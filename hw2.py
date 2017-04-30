a00c4a2517750957724ea7e1f8bb885d8864c81e
balilarder <balilarder@yahoo.com.tw>
2017-04-30 23:18:33 +0800

    fucking hw2

---
+++ hw2.py
@@ -0,0 +1,30 @@
+"""
+hw2: competition
+Design a algorithm to cluster a social network graph
+output: the number of detected cluster and (node, clusterID) pairs
+"""
+
+def mymodel(graph):
+	community_num = 0
+	cluster = {}
+	
+	def method(m):
+		print(m)
+
+
+	return(community_num, cluster)
+	
+
+
+def main():
+	## input
+	print("read txt")
+	graph = {}
+	mymodel(graph)
+	mymodel.method(1)
+	## output
+	print("write txt")
+
+
+if __name__ == '__main__':
+	main()
